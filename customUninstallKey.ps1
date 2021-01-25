38e9484b43e8289f46d9b92f66653d20fb049ed5
Marco <4320849+thunderlight411@users.noreply.github.com>
2021-01-25 11:48:31 +0100

    Update customUninstallKey.ps1

--- customUninstallKey.ps1
+++ customUninstallKey.ps1
@@ -1,15 +1,7 @@
-            ## Create Registry keys for installation Check and provide uninstall key for uninstall
-            If ($ScriptResult.Continue)
-            {
-                Write-Log -Type 'Information' -Message ('Applying registry keys for installation check')
-                New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Force -ErrorAction SilentlyContinue
-                New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Name "DisplayName" -Value "Pearson Market Leader" -PropertyType "String" -Force -ErrorAction SilentlyContinue
-                New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Name "DisplayIcon" -Value "C:\Program Files (x86)\Pearson\intermediate\pearson.ico" -PropertyType "String" -Force -ErrorAction SilentlyContinue
-                New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Name "DisplayVersion" -Value "3.0" -PropertyType "String" -Force -ErrorAction SilentlyContinue
-                New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Name "Publisher" -Value "Pearson Market Leader" -PropertyType "String" -Force -ErrorAction SilentlyContinue
-                New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Name "InstallLocation" -Value "C:\Program Files (x86)\Pearson" -PropertyType "String" -Force -ErrorAction SilentlyContinue
-                New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson" -Name "UninstallString" -Value 'powershell -executionPolicy bypass -WindowStyle Hidden -Command Remove-Item -Path ''C:\Program Files (x86)\Pearson'' -Recurse; remove-item -path HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Pearson' -PropertyType "String" -Force -ErrorAction SilentlyContinue
-
-            # End of installation sequence
-            If ($ScriptResult.Continue) { $ScriptResult.InstallSuccessful = $True }
-            }
+New-Item -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Force -ErrorAction SilentlyContinue
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Name "DisplayName" -Value "APPLICATION_DISPLAYNAME" -PropertyType "String" -Force -ErrorAction SilentlyContinue
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Name "DisplayIcon" -Value "APPLICATION_ICON" -PropertyType "String" -Force -ErrorAction SilentlyContinue
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Name "DisplayVersion" -Value "APPLICATION_VERSION" -PropertyType "String" -Force -ErrorAction SilentlyContinue
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Name "Publisher" -Value "APPLICATION_PUBLISHER" -PropertyType "String" -Force -ErrorAction SilentlyContinue
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Name "InstallLocation" -Value "APPLICAITON_PATH" -PropertyType "String" -Force -ErrorAction SilentlyContinue
+New-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\APPLICATION_NAME" -Name "UninstallString" -Value 'APPLICATION_UNINSTALL_REMOVE_COMMAND' -PropertyType "String" -Force -ErrorAction SilentlyContinue
